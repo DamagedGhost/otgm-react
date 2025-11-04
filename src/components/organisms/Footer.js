abe90cfb51f51ca789053e00d5e9719ff9ad6f09
Degowo-git <diegoparra338@gmail>
2025-11-04 13:52:31 +0000

    footer

--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-center p-3 bg-light mt-5 border-top custom-footer">
+    <footer className="text-center p-3 mt-5 custom-footer">
       <p className="mb-0">&copy; 2024 On The Go Music. Todos los derechos reservados.</p>
     </footer>
   );
